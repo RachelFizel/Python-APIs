e62f07c31d53f9104b30c9b8ef24f8269a335b69
Rachel <rachel.fizel@gmail.com>
2021-02-09 13:31:01 -0600

    added api_key file to gitignore

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "a9cd3775da94217c65921621a995f7d6"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyD7XV5kIvElIxR1dekdeUR_UtUkKJQvu-w"
